cb5ab3029bff337753e16fd4380973233636758d
Lucas Otoni <lotoni.vivo@gmail.com>
2022-09-23 18:06:08 -0300

    fix: requisito 02 mod em user.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,12 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
       display_name: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
